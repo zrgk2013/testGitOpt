a9c13f42ae0a07cd9e407fe27b8e75014a5c50a7
zhangyb-g <zhangyb-g@glodon.com>
2022-06-29 16:07:25 +0800

    1

--- com/zyb/Test.java
+++ com/zyb/Test.java
@@ -3,5 +3,11 @@ package com.zyb;
 public class Test {
     public static void main(String[] args) {
         System.out.println("first c1o1de!");
+        System.out.println("first c1o1de!");
+        System.out.println("first c1o1de!");
+        System.out.println("first c1o1de!");
+        System.out.println("first c1o1de!");
+        System.out.println("first c1o1de!");
+
     }
 }
