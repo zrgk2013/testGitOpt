cb1c9f55deec844075828c3fba9564bd841eff00
zrgk2013 <108391945+zrgk2013@users.noreply.github.com>
2022-06-29 15:31:43 +0800

    Update Test.java

--- com/zyb/Test.java
+++ com/zyb/Test.java
@@ -2,6 +2,6 @@ package com.zyb;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("first code!");
+        System.out.println("first c1ode!");
     }
 }
